3c4ee12cde661b8686ca3a0e05a56c23862b335f
Eduardo Rocha <eduardor626@yahoo.com>
2021-04-14 14:16:41 -0700

    starting lab2

---
+++ Labs/Lab2/main.js
@@ -0,0 +1,9 @@
+function handleCellClick(clickedCellEvent) {
+    alert(clickedCellEvent);
+    var item = document.getElementsByClassName('one').length;
+    var item2 = document.getElementsByClassName('xo').length;
+    alert(item);
+    alert(item2);
+
+}
+
