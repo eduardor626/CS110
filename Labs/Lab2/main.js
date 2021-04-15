ec9b8f906e4e92bc5a403c17c4f81f22ede74523
Eduardo Rocha <eduardor626@yahoo.com>
2021-04-15 14:05:52 -0700

    click on every cell working

--- Labs/Lab2/main.js
+++ Labs/Lab2/main.js
@@ -1,3 +1,4 @@
+//our players
 var playerX = 'X';
 var playerO = 'O';
 
@@ -10,25 +11,28 @@ var gameOver = false;
 var gameBoard = [1,2,3,4,5,6,7,8,9];
 var moves = 1;
 
+//must display these at load time
 document.getElementsByClassName("display_score")[0].innerHTML = "X="+xWins+"\tO="+oWins;
 document.getElementsByClassName("display_player")[0].innerHTML = whoseTurn;
 
+// when a cell is clicked 
 function handleCellClick(clickedCellEvent) {
-    alert(clickedCellEvent);
-    alert(document.getElementById(clickedCellEvent).innerHTML)
+    alert("clicked cell = "+clickedCellEvent);
+    // alert(document.getElementById(clickedCellEvent).innerHTML)
     if(checkValid(clickedCellEvent)){
+        markCell(clickedCellEvent);
         document.getElementById(clickedCellEvent).getElementsByClassName("xo")[0].innerHTML = whoseTurn;
         nextTurn();
     }
-
 }
 
 function checkValid(clickedCellEvent){
     return document.getElementById(clickedCellEvent).getElementsByClassName("xo")[0].innerHTML.length == 0;
 }
 
+//handle the next move and elements that have to change
 function nextTurn(){
-
+    alert(gameBoard);
     moves = moves +1;
     let player = whoseTurn;
     if(player == playerO){
@@ -38,9 +42,16 @@ function nextTurn(){
     }
     document.getElementsByClassName("display_player")[0].innerHTML = whoseTurn;
 
-    if(moves == 9){
+    if(moves > 9){
         gameOver=true;
         alert("Game over!");
     }
+}
 
+function markCell(number){
+    gameBoard[number-1] = whoseTurn;
+}
+
+function resetGame(){
+    location.reload();
 }
