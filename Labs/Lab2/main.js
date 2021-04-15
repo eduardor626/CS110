a3a3b9037f884298acdf33034b64e0138ebd1fcb
Eduardo Rocha <eduardor626@yahoo.com>
2021-04-15 13:13:54 -0700

    adding files

--- Labs/Lab2/main.js
+++ Labs/Lab2/main.js
@@ -1,18 +1,46 @@
+var playerX = 'X';
+var playerO = 'O';
 
+var xWins = 0;
+var oWins = 0;
 
+var whoseTurn = playerX;
+var gameOver = false;
 
+var gameBoard = [1,2,3,4,5,6,7,8,9];
+var moves = 1;
 
+document.getElementsByClassName("display_score")[0].innerHTML = "X="+xWins+"\tO="+oWins;
+document.getElementsByClassName("display_player")[0].innerHTML = whoseTurn;
 
 function handleCellClick(clickedCellEvent) {
     alert(clickedCellEvent);
-    var item = document.getElementsByClassName('one').length;
-    var item2 = document.getElementsByClassName('xo').length;
-    alert(item);
-    document.getElementsByClassName('one')[0].getElementsByClassName('xo')[0].innerHTML = 'X';
-
-    alert(item2);
-
-    // document.getElementsByClassName('one')[0].innerHTML = next;
+    alert(document.getElementById(clickedCellEvent).innerHTML)
+    if(checkValid(clickedCellEvent)){
+        document.getElementById(clickedCellEvent).getElementsByClassName("xo")[0].innerHTML = whoseTurn;
+        nextTurn();
+    }
 
 }
 
+function checkValid(clickedCellEvent){
+    return document.getElementById(clickedCellEvent).getElementsByClassName("xo")[0].innerHTML.length == 0;
+}
+
+function nextTurn(){
+
+    moves = moves +1;
+    let player = whoseTurn;
+    if(player == playerO){
+        whoseTurn = playerX;
+    }else{
+        whoseTurn = playerO;
+    }
+    document.getElementsByClassName("display_player")[0].innerHTML = whoseTurn;
+
+    if(moves == 9){
+        gameOver=true;
+        alert("Game over!");
+    }
+
+}
--- MyExamples/JSIntro/greetings.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("button").onclick = function(){
-    document.getElementById("confirm").innerHTML = "Order Placed!";
-    console.log("placed order")
-}
-
