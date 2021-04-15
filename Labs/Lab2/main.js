d810d1aa55981d2133a7984c49c907a24129446f
Eduardo Rocha <eduardor626@yahoo.com>
2021-04-14 20:31:29 -0700

    have cell click communicating with javascript

--- Labs/Lab2/main.js
+++ Labs/Lab2/main.js
@@ -1,9 +1,18 @@
+
+
+
+
+
 function handleCellClick(clickedCellEvent) {
     alert(clickedCellEvent);
     var item = document.getElementsByClassName('one').length;
     var item2 = document.getElementsByClassName('xo').length;
     alert(item);
+    document.getElementsByClassName('one')[0].getElementsByClassName('xo')[0].innerHTML = 'X';
+
     alert(item2);
+
+    // document.getElementsByClassName('one')[0].innerHTML = next;
 
 }
 
