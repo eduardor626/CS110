69b8d97f795c5fcf183c868ce04b9754f32a5536
Eduardo Rocha <eduardor626@yahoo.com>
2021-04-27 15:43:35 -0700

    have the console outputting 10 tweets every 10 s

--- Labs/Lab3/main.js
+++ Labs/Lab3/main.js
@@ -2,7 +2,8 @@
 
 const url = "http://ec2-54-219-224-129.us-west-1.compute.amazonaws.com:2000/feed/random?q=weather"
 
-var doThisEachTime = window.setInterval(getTweets(), 10000)
+// every 10 seconds
+var doThisEachTime = window.setInterval(getTweets, 10000)
 
 // specify to get the 10 tweets from the server
 function getTweets() {
