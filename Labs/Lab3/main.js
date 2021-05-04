d34251cce0a6432640a87244ae19a9ec8de6cce7
Sameh Fazli <sfazli96@gmail.com>
2021-05-04 12:33:43 -0700

    json data

---
+++ Labs/Lab3/main.js
@@ -0,0 +1,25 @@
+// specify a url, in this case our web server
+
+const url = "http://ec2-54-219-224-129.us-west-1.compute.amazonaws.com:2000/feed/random?q=weather"
+
+// every 10 seconds
+var doThisEachTime = window.setInterval(getTweets, 10000)
+
+var xmlhttp = new XMLHttpRequest();
+
+// specify to get the 10 tweets from the server
+function getTweets() {
+    fetch(url)
+        .then(res => res.json()).then(data => {
+            // do something with data
+            console.log(data);
+            console.log("new iteration");
+            twitter['entities'][1];
+        })
+        .catch(err => {
+            // error catching
+            console.log(err);
+        })
+        return;
+}
+
