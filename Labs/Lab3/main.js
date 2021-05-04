4bb9d2d928875559b4f6bbdb2edba9431c5cd6de
Eduardo Rocha <eduardor626@yahoo.com>
2021-05-04 14:39:31 -0700

    fixing center content

--- Labs/Lab3/main.js
+++ Labs/Lab3/main.js
@@ -38,7 +38,7 @@ function createTweet(tweets){
     //handle the creation of the tweet on the JS container
     var tweetDate = tweets.user.created_at;
     tweetDate = tweetDate.split(' ');
-    var formatTweetDate = tweetDate[0] +" "+tweetDate[1]+" "+tweetDate[2]+ " "+tweetDate[5];
+    var formatTweetDate = tweetDate[1]+" "+tweetDate[2]+ " "+tweetDate[5];
     var userName = tweets.user.name;
     var userInformation = " @"+tweets.user.screen_name+" "+formatTweetDate;
     
@@ -51,23 +51,28 @@ function createTweet(tweets){
     var gridItem = document.createElement("div");
     var tweetImage = document.createElement("img");
     tweetImage.src = tweets.user.profile_image_url;
-    var tweetNameLabel = document.createElement("label");
-    var tweetMessage = document.createElement("div");
-    var tweetDateLabel = document.createElement("label");
+    var otherBorder = document.createElement("div");
+    var userLabel = document.createElement("p");
+    var dateLabel = document.createElement("span") 
+    var tweetMessage = document.createElement("p");
 
+    //specifying which classes these elements belong to
     gridItem.classList.add("grid-item");
     tweetImage.classList.add("grid-it");
-    tweetNameLabel.classList.add("NameText");
+    otherBorder.classList.add("otherBorder");
+    userLabel.classList.add("user-info");
+    dateLabel.classList.add("Name-Date")
     tweetMessage.classList.add("tweet-message");
-    tweetDateLabel.classList.add("Name-Date");
 
     gridItem.appendChild(tweetImage);
-    tweetNameLabel.appendChild(user);
+    userLabel.appendChild(user);
+    dateLabel.appendChild(tweetUserInfo);
     tweetMessage.appendChild(tweetText);
-    tweetNameLabel.appendChild(tweetMessage);
-    gridItem.appendChild(tweetNameLabel);
-    tweetDateLabel.appendChild(tweetUserInfo);
-    gridItem.appendChild(tweetDateLabel);
+    userLabel.appendChild(dateLabel);
+    otherBorder.appendChild(userLabel);
+
+    otherBorder.appendChild(tweetMessage);
+    gridItem.appendChild(otherBorder);
     
     tweetContainer.prepend(gridItem);
 
