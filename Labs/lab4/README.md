9236739d10bb9945a332a6773490798264c98c99
Eduardo Rocha <eduardor626@yahoo.com>
2021-05-17 12:53:07 -0700

    adding home page sign on and enter room

--- Labs/lab4/README.md
+++ Labs/lab4/README.md
@@ -3,10 +3,19 @@
 ## Project Description
 <!-- you can include known bugs, design decisions, external references used... -->
 
+
 ## Ethics Questions
 
 ### Question 1
 
 > Give two possible chatroom moderation features and the reasons that you should implement each one
+
+```
+1) 
+
+2) 
+
+
+```
 
 <!-- Put your answer to question 1 here -->
--- Labs/lab4/controllers/home.js
+++ Labs/lab4/controllers/home.js
@@ -2,9 +2,16 @@
 
 // Example for handle a get request at '/' endpoint.
 
-function getHome(request, response){
-  // do any work you need to do, then
-  response.render('home', {title: 'home'});
+function getHome(request, response) {
+    // do any work you need to do, then
+
+    // .render() function is used to render a view and sends 
+    // the rendered HTML string to the client.
+
+    //the first param is the .hbs we are accessing or rendering
+    //the second is the elements i believe
+    response.render('home', { title: 'home' });
+
 }
 
 module.exports = {
