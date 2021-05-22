361393902397efa63dceed795d7905855bcb160b
sfazli96 <sfazli96@gmail.com>
2021-05-22 02:44:40 -0700

    Combined both branch, error from get/post

--- Labs/lab4/server.js
+++ Labs/lab4/server.js
@@ -8,7 +8,6 @@ const mongoose = require('mongoose');
 const config = require('config');
 const Room = require('./models/Rooms');
 
-
 const path = require('path');
 const socketio = require('socket.io');
 const http = require('http');
@@ -22,11 +21,14 @@ const app = express();
 const Server = http.createServer(app);
 const io = socketio(Server);
 const port = 8080;
+
+
 //connecting to DB
 const db = config.get('mongoURI');
 
+
 mongoose
-    .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true})
+    .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => console.log('MongoDB connected..'))
     .catch(err => console.log(err));
 
@@ -35,6 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // If you choose not to use handlebars as template engine, you can safely delete the following part and use your own way to render content
 // view engine setup
@@ -67,7 +70,7 @@ io.on('connection', socket => {
 })
 
 
-// TODO: Add server side code
+
 // dummy way to see all the rooms and do things with it or display on front end
 app.get('/getRooms', function(req, res) {
     Room.find().lean().then(items => {
@@ -75,9 +78,12 @@ app.get('/getRooms', function(req, res) {
     });
 });
 
+// TODO: Add server side code
+
 // Create controller handlers to handle requests at each endpoint
 app.get('/', homeHandler.getHome);
 app.get('/:roomName', roomHandler.getRoom);
+
 
 // respond to /roomName
 app.post('/roomName', (req, res) => {
@@ -95,6 +101,7 @@ app.post('/roomName', (req, res) => {
         res.redirect(newRoom.name);
     }
 });
+
 
 
 // NOTE: This is the sample server.js code we provided, feel free to change the structures
